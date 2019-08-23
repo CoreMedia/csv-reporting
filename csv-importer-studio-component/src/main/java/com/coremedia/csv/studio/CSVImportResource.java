9e06b87215548146083d6ae15c4fd093bfdc4b68
Alia Robinson <alia.robinson@coremedia.com>
2019-08-23 10:29:17 +0200

    Rewrite import endpoint to use CSVConfig

--- csv-importer-studio-component/src/main/java/com/coremedia/csv/studio/CSVImportResource.java
+++ csv-importer-studio-component/src/main/java/com/coremedia/csv/studio/CSVImportResource.java
@@ -4,17 +4,26 @@ import com.coremedia.cap.content.ContentRepository;
 import com.coremedia.cap.user.Group;
 import com.coremedia.cap.user.User;
 import com.coremedia.cap.user.UserRepository;
+import com.coremedia.csv.common.CSVConfig;
+import com.coremedia.csv.importer.CSVParserHelper;
 import com.coremedia.csv.importer.CSVUploader;
 import com.sun.jersey.core.header.FormDataContentDisposition;
 import com.sun.jersey.multipart.FormDataBodyPart;
 import com.sun.jersey.multipart.FormDataParam;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -27,6 +36,12 @@ public class CSVImportResource {
    * The content repository from which to retrieve content.
    */
   private ContentRepository contentRepository;
+
+  /**
+   * Configuration mapping CSV headers to content properties
+   */
+  private CSVConfig csvConfig;
+
   /**
    * Flag indicating whether access to this endpoint should be restricted to authorized groups only
    */
@@ -38,12 +53,26 @@ public class CSVImportResource {
   private List<String> authorizedGroups;
 
   /**
+   * Import process logger.
+   */
+  private static Logger logger = LoggerFactory.getLogger(CSVImportResource.class);
+
+  /**
    * Sets the content repository.
    *
    * @param contentRepository the content repository to set
    */
   public void setContentRepository(ContentRepository contentRepository) {
     this.contentRepository = contentRepository;
+  }
+
+  /**
+   * Sets the CSV configuration
+   *
+   * @param csvConfig the csv configuration to set
+   */
+  public void setCsvConfig(CSVConfig csvConfig) {
+    this.csvConfig = csvConfig;
   }
 
   /**
@@ -66,13 +95,19 @@ public class CSVImportResource {
 
   @POST
   @Consumes({"multipart/form-data"})
-  public Response importCSV(@HeaderParam("site") String siteId, @HeaderParam("folderUri") String folderUri, @FormDataParam("contentName") String contentName, @FormDataParam("file") InputStream inputStream, @FormDataParam("file") FormDataContentDisposition fileDetail, @FormDataParam("file") FormDataBodyPart fileBodyPart) {
+  public Response importCSV(@HeaderParam("site") String siteId, @HeaderParam("folderUri") String folderUri, @FormDataParam("contentName") String contentName, @FormDataParam("file") InputStream inputStream, @FormDataParam("file") FormDataContentDisposition fileDetail, @FormDataParam("file") FormDataBodyPart fileBodyPart) throws IOException {
   // Check that the user is a member of the requisite group
     if (restrictToAuthorizedGroups && !isAuthorized()) {
       return Response.status(Response.Status.UNAUTHORIZED).build();
     }
-    CSVUploader csvuploader = new CSVUploader();
-    csvuploader.runFromRequest(inputStream);
+
+    boolean autoPublish = false;
+    String template = "default";
+    BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+    CSVParser parser = new CSVParser(reader, CSVFormat.EXCEL.withHeader());
+    CSVParserHelper handler = new CSVParserHelper(autoPublish, contentRepository, logger);
+    handler.parseCSV(parser, csvConfig.getReportHeadersToContentProperties(template));
+
     return Response.status(Response.Status.OK).build();
   }
 
