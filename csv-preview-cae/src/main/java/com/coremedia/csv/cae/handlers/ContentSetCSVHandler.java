ba0f6ccdaa44d4edb6e94ab7c44c1bf68a5864bf
Kevin Cherniawski <kevin.cherniawski@coremedia.com>
2020-09-02 12:46:28 -0400

    There are Spring 403 issues that crop up when doing post requests with Spring Enabled Web Security. These revolve around CSRF tokens to validate against cross-site vulnerabilities. For the CSV Reporter, which is being called via the Studio into the CAE, we need to disable CSRF for now. A bit of a workaround but this will do the job for now.

--- csv-preview-cae/src/main/java/com/coremedia/csv/cae/handlers/ContentSetCSVHandler.java
+++ csv-preview-cae/src/main/java/com/coremedia/csv/cae/handlers/ContentSetCSVHandler.java
@@ -1,17 +1,12 @@
 package com.coremedia.csv.cae.handlers;
 
 import com.coremedia.objectserver.web.links.Link;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.net.URLDecoder;
-
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Handles a request to export a content report in CSV format. The request must contain a list of content IDs.
@@ -35,8 +30,7 @@ public class ContentSetCSVHandler extends BaseCSVHandler {
    * @param response the HTTP Response, used for building content beans and writing CSV
    * @throws IOException if an error occurs writing the CSV
    */
-  @RequestMapping(value = CSV_LINK_PATTERN,
-          method = POST,
+  @PostMapping(value = CSV_LINK_PATTERN,
           produces = "text/csv",
           consumes = "application/json")
   @ResponseBody
