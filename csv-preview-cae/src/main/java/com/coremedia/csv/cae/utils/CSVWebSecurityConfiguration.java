22f69ee118fdb9b31ce4b1a115c38aba4bf57896
Sascha Leyer <sascha.leyer@coremedia.com>
2021-01-27 16:20:22 +0100

    Properly disabled CSRF-security and streamlined CORS configuration.
    The configuration caused the remoteUrlHandler of the CAE (used by the remoteLink-graphql-field of the headless-server) to not be accessible.

--- csv-preview-cae/src/main/java/com/coremedia/csv/cae/utils/CSVWebSecurityConfiguration.java
+++ csv-preview-cae/src/main/java/com/coremedia/csv/cae/utils/CSVWebSecurityConfiguration.java
@@ -1,6 +1,5 @@
 package com.coremedia.csv.cae.utils;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,8 +7,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 /**
  * There are Spring 403 issues that crop up when doing post requests with Spring Enabled Web Security. These revolve
@@ -22,16 +19,14 @@ public class CSVWebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.cors().and().csrf().and().headers().disable();
+    http.csrf().disable();
+    http.headers().disable();
+    http.cors().configurationSource(corsConfigurationSource());
   }
 
-  @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("*"));
-    configuration.setAllowedHeaders(Arrays.asList("*"));
-    configuration.setAllowCredentials(true);
+    configuration.applyPermitDefaultValues();
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
