7f6c58f8497e0c76c1fb042c7d268358cec918cb
Henning Saul <henning.saul@coremedia.com>
2023-11-10 16:12:02 -0500

    Support for custom LayoutVariant property field

--- csv-preview-cae/src/main/java/com/coremedia/csv/cae/utils/ContentSetCSVUtil.java
+++ csv-preview-cae/src/main/java/com/coremedia/csv/cae/utils/ContentSetCSVUtil.java
@@ -1,5 +1,7 @@
 package com.coremedia.csv.cae.utils;
 
+import com.coremedia.blueprint.common.contentbeans.CMLinkable;
+import com.coremedia.blueprint.common.contentbeans.CMViewtype;
 import com.coremedia.cap.content.Content;
 
 import java.util.List;
@@ -11,7 +13,8 @@ import java.util.Map;
 public class ContentSetCSVUtil extends BaseCSVUtil {
 
   /**
-   * As this request type does not require custom properties to be processed, this method is a no-op.
+   * If additional custom report/property fields are required, feel free to extend this class, overwrite populateCustomPropertyFields
+   * and replace the contentSetCSVUtil bean.
    *
    * @param csvRecord  the CSV record to which to populate the properties of the content
    * @param content    the content from which the property values will be parsed
@@ -21,7 +24,16 @@ public class ContentSetCSVUtil extends BaseCSVUtil {
   @Override
   protected void populateCustomPropertyFields(Map<String, String> csvRecord, Content content,
                                               List<String> headerList) {
-    // no-op
+    // custom property to include Layout Variant name
+    if(headerList.contains("LayoutVariant")) {
+      String layoutProperty = "";
+      if(content.getType().isSubtypeOf(CMLinkable.NAME)) {
+        Content viewtype = content.getLink(CMLinkable.VIEWTYPE);
+        if(viewtype != null)
+          layoutProperty = viewtype.getString(CMViewtype.LAYOUT);
+      }
+      csvRecord.put("LayoutVariant", layoutProperty);
+    }
   }
 
 }
