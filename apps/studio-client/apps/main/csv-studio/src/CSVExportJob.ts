dc76d9891b87f687983040613a9bf9dd3b407210
Henning Saul <henning.saul@coremedia.com>
2024-05-01 16:34:14 -0400

    Implement BackgroundJob so running job its visualized in Studio

--- apps/studio-client/apps/main/csv-studio/src/CSVExportJob.ts
+++ apps/studio-client/apps/main/csv-studio/src/CSVExportJob.ts
@@ -1,7 +1,12 @@
 import RemoteJobBase from "@coremedia/studio-client.cap-rest-client-impl/common/impl/RemoteJobBase";
 import JobContext from "@coremedia/studio-client.cap-rest-client/common/JobContext";
+import ValueExpression from "@coremedia/studio-client.client-core/data/ValueExpression";
+import ValueExpressionFactory from "@coremedia/studio-client.client-core/data/ValueExpressionFactory";
+import BackgroundJob from "@coremedia/studio-client.main.editor-components/sdk/jobs/BackgroundJob";
+import {AnyFunction} from "@jangaroo/runtime/types";
+import {mixin} from "@jangaroo/runtime";
 
-class CSVExportJob extends RemoteJobBase {
+class CSVExportJob extends RemoteJobBase implements BackgroundJob {
   static readonly #JOB_TYPE_CSV_REPORTER_EXPORT: string = "csvReporterExport";
 
   protected params: any;
@@ -30,6 +35,23 @@ class CSVExportJob extends RemoteJobBase {
     return false;
   }
 
+  getNameExpression():ValueExpression {
+    return ValueExpressionFactory.createFromValue("A CSVExportJob");
+  }
+
+  getIconClsExpression(): ValueExpression {
+    return ValueExpressionFactory.createFromValue("xliff");
+  }
+
+  getErrorHandler(): AnyFunction {
+    return null;
+  }
+
+  getSuccessHandler(): AnyFunction {
+    return null;
+  }
+
 }
+mixin(CSVExportJob, BackgroundJob);
 
 export default CSVExportJob;
